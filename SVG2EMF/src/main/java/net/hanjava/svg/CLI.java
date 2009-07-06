94d54c80f369e437fb48c8a438c3b118391e65fe
behumble <behumble@36f2d2b9-a042-0410-bf1b-77892af52abc>
2009-07-06 02:59:03 +0000

    Command line interface

---
+++ SVG2EMF/src/main/java/net/hanjava/svg/CLI.java
@@ -0,0 +1,39 @@
+package net.hanjava.svg;
+
+import java.io.File;
+import java.net.URL;
+
+import net.hanjava.svg.SVG2EMF;
+
+public class CLI {
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			printUsage();
+			System.exit(-1);
+		}
+
+		String arg = args[0];
+		boolean isFilePath = !arg.contains(":/");
+		URL inputUrl = null;
+		if (isFilePath) {
+			inputUrl = new File(arg).toURL();
+		} else {
+			inputUrl = new URL(arg);
+		}
+
+		String inputPath = inputUrl.getPath();
+		String fileName = inputPath.substring( inputPath.lastIndexOf('/') + 1 );
+
+		System.out.println("[SVG2EMF] input : "+inputUrl);
+		File output = new File(fileName+".emf");
+		SVG2EMF.convert(inputUrl.toString(), output);
+		System.out.println("[SVG2EMF] output : "+output.getAbsolutePath());
+	}
+
+	private static void printUsage() {
+		String msg = "Usage: svg2emf input.svg";
+		System.out.println(msg);
+		System.out.println("input can be any local path or remote URL");
+		System.out.println("Contact: behumble@hanjava.net");
+	}
+}
