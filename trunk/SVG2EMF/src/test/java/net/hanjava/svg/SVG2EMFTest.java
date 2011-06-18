d445c605721a7548277d42a22b1de8dcb7deedfc
behumble@gmail.com <behumble@gmail.com@36f2d2b9-a042-0410-bf1b-77892af52abc>
2011-06-18 02:23:47 +0000

    test for both headless and headful

--- trunk/SVG2EMF/src/test/java/net/hanjava/svg/SVG2EMFTest.java
+++ trunk/SVG2EMF/src/test/java/net/hanjava/svg/SVG2EMFTest.java
@@ -6,9 +6,19 @@ import java.io.IOException;
 import junit.framework.TestCase;
 
 public class SVG2EMFTest extends TestCase {
-    public void testConvert() throws IOException {
+    private void doConvert() throws IOException {
         String svgUrl = "http://upload.wikimedia.org/wikipedia/en/7/7f/Mickey_Mouse.svg";
         File emfFile = new File("mickey.emf");
         SVG2EMF.convert(svgUrl, emfFile);
+    }
+
+    public void testConvertHeadless() throws IOException {
+    	System.setProperty("java.awt.headless", "true");
+    	doConvert();
+    }
+
+    public void testConvertHeadful() throws IOException {
+    	System.setProperty("java.awt.headless","false");
+    	doConvert();
     }
 }
